67d93db501dffd702dbb3498ecfc694f65239184
smidovic <msmidovic@gmail.com>
2021-11-15 20:54:09 +0100

    funkcia na zdvojenie pismen

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -6,7 +6,13 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            string dvojite = string.Empty;
+            for (int i = 0; i < original.Length; i++)
+            {
+                string a = Convert.ToString(original[i]) + original[i];
+                dvojite = dvojite + a;
+            }
+            return dvojite;
         }
     }
 }
